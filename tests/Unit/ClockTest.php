99081685fb6a2952333e981544fdc40755b2edb1
ಠ_ಠ <easteregg@verfriemelt.org>
2024-01-01 20:33:17 +0100

    add unittests for Clock

---
+++ tests/Unit/ClockTest.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace timer\tests;
+
+use DateMalformedStringException;
+use DateTimeImmutable;
+use PHPUnit\Framework\TestCase;
+use timer\Domain\Clock;
+use verfriemelt\wrapped\_\Clock\MockClock;
+
+class ClockTest extends TestCase
+{
+    public function test_modify(): void
+    {
+        static::expectException(DateMalformedStringException::class);
+
+        $clock = new Clock();
+        $clock->fromString('foobar');
+    }
+
+    public function test_now(): void
+    {
+        $time = new DateTimeImmutable('2022-04-01');
+        $clock = new Clock(new MockClock($time));
+
+        static::assertSame(
+            $time->modify('-1 day')->format('Y-m-d'),
+            $clock->fromString('yesterday')->format('Y-m-d'),
+        );
+    }
+}
